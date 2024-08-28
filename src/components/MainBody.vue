f9b55a47adc91bac19b40e1c81391afa8bda8911
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-28 16:30:40 +0530

    create nav and home

---
+++ src/components/MainBody.vue
@@ -0,0 +1,156 @@
+<template>
+    <section class="flex w-10/12 flex-col gap-6 items-center justify-center">
+
+        <div class="flex flex-row flex-wrap items-start justify-between bg-white">
+            <div class="w-96 h-full mb-10 shadow rounded-2xl text-[#1c2022] border border-[#dbe5e6]"
+                v-for="solution in solutions.slice().reverse()" v-bind:key="solution.id">
+                <!-- Project Preview -->
+                <section class="rounded-t-2xl overflow-hidden innerShadow">
+                    <img class="rounded-t-2xl hover:scale-110 transitionVue" v-bind:src="solution.projectPreview"
+                        alt={{solution.projectName}} />
+                </section>
+
+                <div class="p-6 flex text-left h-[300px]">
+                    <!-- Project Info -->
+                    <section class="flex flex-col justify-between gap-4">
+
+                        <div class="w-full h-auto flex flex-col gap-4">
+                            <!-- Project Name -->
+                            <p class="barlow text-2xl font-medium text-[#1c2022] hover:underline transitionVue capitalize">{{
+                                solution.projectName }}</p>
+
+                            <!-- Project Describtion -->
+                            <section class="text-[#737373] text-lg barlow leading-7">
+                                <p>{{ solution.projectDescribtion }}</p>
+                            </section>
+                        </div>
+
+                        <!-- Project Stack -->
+                        <!-- <section class="flex flex-wrap gap-2">
+                            <p class="project-stack" v-for="projectStack in solution.projectStack"
+                                v-bind:class="projectStack" v-bind:key="projectStack">
+                                {{ projectStack }}
+                            </p>
+                        </section> -->
+
+                        <!-- Links -->
+                        <section class="w-full h-auto flex flex-col">
+                            <ul class="flex flex-row gap-4">
+                                <li class="link-btn">
+                                    <a v-bind:href="solution.liveLink" target="_blank">Live</a>
+                                </li>
+                                <li class="link-btn">
+                                    <a v-bind:href="solution.repoLink" target="_blank">Repo</a>
+                                </li>
+                                <li class="link-btn">
+                                    <a v-bind:href="solution.projectLink" target="_blank">Try Out</a>
+                                </li>
+                            </ul>
+                        </section>
+
+                    </section>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'MainBody',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            solutions: [
+                {
+                    id: 1,
+                    projectName: "Stats Preview Card Component",
+                    projectDescribtion:
+                        "This is a great small challenge to help get you used to building to a design. There's no JS in this project, so you'll be able to focus on your HTML & CSS skills.",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/stats-preview-card-component.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "https://github.com/catherineisonline/stats-preview-card-component-frontendmentor",
+                },
+                {
+                    id: 2,
+                    projectName: "Order summary component",
+                    projectDescribtion:
+                        "A perfect project for newbies who are starting to build confidence with layouts!",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/order-summary-component.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "",
+                },
+                {
+                    id: 3,
+                    projectName: "Profile card component",
+                    projectDescribtion:
+                        "This is a perfect challenge to test your layout skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet!",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/profile-card-component.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "",
+                },
+                {
+                    id: 4,
+                    projectName: "Single price grid component",
+                    projectDescribtion:
+                        "In this challenge, you will build out the pricing component to the designs provided. This is perfect for beginners and people who want to complete a smaller challenge.",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/single-price-grid-component.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "",
+                },
+                {
+                    id: 5,
+                    projectName: "3-column preview card component",
+                    projectDescribtion:
+                        "This challenge is perfect if you're just getting started. The shift between the layouts will be a nice test if you're new to building responsive projects.",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/3-column-preview-card-component.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "",
+                },
+                {
+                    id: 6,
+                    projectName: "Social proof section",
+                    projectDescribtion:
+                        "This project will test your layout skills. If you're starting to get confident with Flexbox or Grid, this will provide a nice challenge!",
+                    projectStack: ["HTML", "CSS"],
+                    projectPreview: require("@/assets/images/social-proof-section.jpeg"),
+                    liveLink:
+                        "",
+                    projectLink:
+                        "",
+                    repoLink:
+                        "",
+                },
+            ],
+        };
+    },
+};
+</script>
---
+++ src/components/NavbarCmp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full flex flex-col items-center justify-center shadow">
+    <header class="w-10/12 h-20 flex flex-row justify-between items-center">
+
+      <div class="flex flex-row gap-4 barlow">
+        <img src="../assets/logo-desktop.svg" alt="" class="h-10 p-1">
+      </div>
+      <div class="flex flex-row gap-2.5 items-center">
+        <img src="../assets/profile.jpeg" alt="" class="w-10 h-10 rounded-full">
+        <img src="../assets/arrow-down.png" alt="" class="h-3.5 hover:rotate-180">
+      </div>
+
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarCmp',
+  props: {
+    msg: String
+  },
+  methods: {
+    click() {
+      document.querySelector(".main-menu").classList.toggle("show-menu");
+    },
+    removeMenu() {
+      document.querySelector(".main-menu").classList.remove("show-menu");
+    },
+  },
+};
+</script>
